81770f118d8d3a21b0ab78bb527edbfdc2c4e8d8
EddieCornelious <eulerissmart@gmail.com>
2019-04-17 10:11:14 -0400

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import "../styles/App.scss";
+import React from "react";
+import MainSection from "./MainSection.js";
+
+class App extends React.Component {
+  render() {
+    return <MainSection />;
+  }
+}
+
+export default App;
---
+++ src/components/MainSection.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class MainSection extends React.Component {
+  render() {
+    return <div>lolll</div>;
+  }
+}
+
+export default MainSection;
