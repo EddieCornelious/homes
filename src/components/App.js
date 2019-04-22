094c699a241b5dd75bcef3c47462efe2b4f45765
EddieCornelious <eulerissmart@gmail.com>
2019-04-22 19:06:27 -0400

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,9 @@
 import "../styles/App.scss";
 import React from "react";
-import MainSection from "./MainSection.js";
-import WelcomeSection from "./WelcomeSection.js";
-import MainMenuSection from "./MainMenuSection.js";
-import DessertSection from "./DessertSection.js";
-import DrinkSection from "./DrinkSection.js";
-import Reservation from "./Reservation.js";
-import Footer from "./Footer.js";
+
 class App extends React.Component {
   render() {
-    return (
-      <div>
-        <MainSection />
-        <WelcomeSection />
-        <MainMenuSection />
-        <DessertSection />
-        <DrinkSection />
-        <Reservation />
-        <Footer />
-      </div>
-    );
+    return <div>lol</div>;
   }
 }
 
