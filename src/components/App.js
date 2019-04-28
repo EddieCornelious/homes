454b968c914c9a27f9317caafbff5f6605190815
EddieCornelious <eulerissmart@gmail.com>
2019-04-28 14:03:23 -0400

    iniaial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,9 @@
 import "../styles/App.scss";
 import React from "react";
-import Header from "./Header.js";
-import WhySection from "./WhySection.js";
-import CoachesSection from "./CoachesSection.js";
-import PlansSection from "./PlansSection.js";
-import TestimonySection from "./TestimonySection.js";
-import AppointmentSection from "./AppointmentSection.js";
-import Footer from "./Footer.js";
 
 class App extends React.Component {
   render() {
-    return (
-      <div>
-        <div className="hero">
-          <div className="hero__text__wrap">
-            <h1>Fitness & Health is a Mentality</h1>
-            <p>Let us help you cultivate a fitness lifestyle</p>
-            <a href="#">Start your journey</a>
-          </div>
-        </div>
-        <WhySection />
-        <CoachesSection />
-        <PlansSection />
-        <TestimonySection />
-        <AppointmentSection />
-        <Footer />
-      </div>
-    );
+    return <div>LOLLLL</div>;
   }
 }
 
