a168f8ab6b871ae4ed1e7fd99c157e7e7c46a140
EddieCornelious <eulerissmart@gmail.com>
2019-05-03 13:31:12 -0400

    spruced up featured section

--- src/components/FeaturedSection.js
+++ src/components/FeaturedSection.js
@@ -1,49 +1,51 @@
 import "../styles/FeaturedSection.scss";
 import React from "react";
-import bg1 from "../images/house1.jpg";
+import { homes } from "./data.js";
 
-const PropertyCard = () => {
+const PropertyCard = props => {
+  const data = homes[props.id];
   return (
     <div className="property__card">
       <div
-        style={{ backgroundImage: "url(" + bg1 + ")" }}
+        style={{ backgroundImage: "url(" + data.img + ")" }}
         className="property__card__top"
       >
-        <a href="#">forsale</a>
+        <a href="#">{data.status === 1 ? "for sale" : "for rent"}</a>
       </div>
       <div className="property__card__bottom">
         <div className="card__bottom__location">
-          <h2>1890 NOrth Cambridge</h2>
+          <h2>{data.address}</h2>
           <p>
-            <i class="fas fa-map-marker-alt" /> Beveryl Hills, CA 90210
+            <i class="fas fa-map-marker-alt" />
+            {data.city}
           </p>
         </div>
 
         <div className="card__bottom__row">
           <p>
-            <i class="fa fa-th-large" /> 800 sq ft.
+            <i class="fa fa-th-large" /> {data.size + "sq ft."}
           </p>
           <p>
-            <i class="fa fa-car" /> 2 garages
+            <i class="fa fa-car" /> {data.garage + " garages"}
           </p>
 
           <p>
-            <i class="fa fa-bed" /> 3 bedrooms
+            <i class="fa fa-bed" /> {data.bed + " bedrooms"}
           </p>
           <p>
-            <i class="fa fa-shower" /> 1 bathrooms
+            <i class="fa fa-shower" /> {data.bath + " bathrooms"}
           </p>
         </div>
 
         <div className="card__bottom__row">
           <p>
-            <i class="fa fa-user" /> James Hunkle
+            <i class="fa fa-user" /> {data.realator}
           </p>
           <p>
-            <i class="fa fa-clock" /> 1 days ago
+            <i class="fa fa-clock" /> {data.posted + " days ago"}
           </p>
         </div>
-        <div className="bottom__button">$150,000</div>
+        <div className="bottom__button">{"$" + data.price}</div>
       </div>
     </div>
   );
@@ -55,22 +57,22 @@ const FeaturedSection = () => {
         <h1>Featured Section</h1>
         <div className="row no-gutters">
           <div className="col-md-4">
-            <PropertyCard />
+            <PropertyCard id="0" />
           </div>
           <div className="col-md-4">
-            <PropertyCard />
+            <PropertyCard id="1" />
           </div>
           <div className="col-md-4">
-            <PropertyCard />
+            <PropertyCard id="2" />
           </div>
           <div className="col-md-4">
-            <PropertyCard />
+            <PropertyCard id="3" />
           </div>
           <div className="col-md-4">
-            <PropertyCard />
+            <PropertyCard id="4" />
           </div>
           <div className="col-md-4">
-            <PropertyCard />
+            <PropertyCard id="5" />
           </div>
         </div>
       </div>
---
+++ src/components/data.js
@@ -0,0 +1,86 @@
+import img0 from "../images/house1.jpg";
+import img1 from "../images/house2.jpg";
+import img2 from "../images/house3.jpg";
+import img3 from "../images/house4.jpg";
+import img4 from "../images/house5.jpg";
+import img5 from "../images/house6.jpg";
+export const homes = {
+  0: {
+    img: img0,
+    status: 1,
+    size: 700,
+    garage: 2,
+    bed: 2,
+    bath: 3,
+    realator: "James Funkle",
+    posted: 1,
+    address: "1890 Cambridge Ave",
+    city: "Beverly Hills, CA 90210",
+    price: "210,000"
+  },
+  1: {
+    img: img1,
+    status: 2,
+    size: 350,
+    garage: 2,
+    bed: 2,
+    bath: 3,
+    realator: "James Funkle",
+    posted: 30,
+    address: "1512 Lockloal Dr.",
+    city: "Hollywood, CA 98724",
+    price: "489,000"
+  },
+  2: {
+    img: img2,
+    status: 2,
+    size: 300,
+    garage: 4,
+    bed: 4,
+    bath: 3,
+    realator: "Carlisle James",
+    posted: 20,
+    address: "1982 BuckHead Lane",
+    city: "Brockton, NJ 67384",
+    price: "650,000"
+  },
+  3: {
+    img: img3,
+    status: 2,
+    size: 200,
+    garage: 1,
+    bed: 3,
+    bath: 3,
+    realator: "Frank Turek",
+    posted: 3,
+    address: "1512 Lockloal Dr.",
+    city: "Hollywood, CA 98724",
+    price: "550,000"
+  },
+  4: {
+    img: img4,
+    status: 2,
+    size: 800,
+    garage: 3,
+    bed: 3,
+    bath: 2,
+    realator: "James Funkle",
+    posted: 1,
+    address: "152 Shackle",
+    city: "Columbus, OH 43215",
+    price: "150,000"
+  },
+  5: {
+    img: img5,
+    status: 2,
+    size: 500,
+    garage: 2,
+    bed: 2,
+    bath: 3,
+    realator: "Terry Crews",
+    posted: 15,
+    address: "167 Packard ST.",
+    city: "Hollywood, CA 98724",
+    price: "245,000"
+  }
+};
