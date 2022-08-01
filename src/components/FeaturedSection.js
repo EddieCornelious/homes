977e537477ea1226823d07356cfbb4c0c25b53bc
Eddie <eulerissmart@gmail.com>
2022-08-01 00:57:17 -0400

    improved responsiveness

--- src/components/FeaturedSection.js
+++ src/components/FeaturedSection.js
@@ -1,9 +1,9 @@
-import "../styles/FeaturedSection.scss";
-import React from "react";
-import { homes } from "./data.js";
-import LazyLoad from "react-lazyload";
+import '../styles/FeaturedSection.scss';
+import React from 'react';
+import { homes } from './data.js';
+import LazyLoad from 'react-lazyload';
 
-const PropertyCard = props => {
+const PropertyCard = (props) => {
   const data = homes[props.id];
   return (
     <div className="property__card">
@@ -12,7 +12,7 @@ const PropertyCard = props => {
           <img alt="property image" src={data.img} />
         </LazyLoad>
         <a className="availability" href="#">
-          {data.status === 1 ? "for sale" : "for sale"}
+          {data.status === 1 ? 'for sale' : 'for sale'}
         </a>
         <a
           onClick={() => props.toggleDetail(props.id)}
@@ -32,17 +32,17 @@ const PropertyCard = props => {
 
         <div className="card__bottom__row">
           <p>
-            <i className="fa fa-th-large" /> {data.size + "sq ft."}
+            <i className="fa fa-th-large" /> {data.size + 'sq ft.'}
           </p>
           <p>
-            <i className="fa fa-car" /> {data.garage + " garages"}
+            <i className="fa fa-car" /> {data.garage + ' garages'}
           </p>
 
           <p>
-            <i className="fa fa-bed" /> {data.bed + " bedrooms"}
+            <i className="fa fa-bed" /> {data.bed + ' bedrooms'}
           </p>
           <p>
-            <i className="fa fa-shower" /> {data.bath + " bathrooms"}
+            <i className="fa fa-shower" /> {data.bath + ' bathrooms'}
           </p>
         </div>
 
@@ -51,36 +51,36 @@ const PropertyCard = props => {
             <i className="fa fa-user" /> {data.realator}
           </p>
           <p>
-            <i className="fa fa-clock" /> {data.posted + " days ago"}
+            <i className="fa fa-clock" /> {data.posted + ' days ago'}
           </p>
         </div>
       </div>
-      <div className="bottom__button">{"$" + data.price}</div>
+      <div className="bottom__button">{'$' + data.price}</div>
     </div>
   );
 };
-const FeaturedSection = props => {
+const FeaturedSection = (props) => {
   return (
     <section className="featured__section">
       <div className="container">
         <h1>Featured Section</h1>
         <div className="row">
-          <div className="col-xs-12 col-sm-6 col-md-4">
+          <div className="col-xxs-12 col-xs-6 col-sm-6 col-md-4">
             <PropertyCard toggleDetail={props.toggleDetail} id={0} />
           </div>
-          <div className="col-xs-12 col-sm-6 col-md-4">
+          <div className="col-xxs-12 col-xs-6 col-sm-6 col-md-4">
             <PropertyCard toggleDetail={props.toggleDetail} id={1} />
           </div>
-          <div className="col-xs-12 col-sm-6 col-md-4">
+          <div className="col-xxs-12 col-xs-6 col-sm-6 col-md-4">
             <PropertyCard toggleDetail={props.toggleDetail} id={2} />
           </div>
-          <div className="col-xs-12 col-sm-6 col-md-4">
+          <div className="col-xxs-12 col-xs-6 col-sm-6 col-md-4">
             <PropertyCard toggleDetail={props.toggleDetail} id={3} />
           </div>
-          <div className="col-xs-12 col-sm-6 col-md-4">
+          <div className="col-xxs-12 col-xs-6 col-sm-6 col-md-4">
             <PropertyCard toggleDetail={props.toggleDetail} id={4} />
           </div>
-          <div className="col-xs-12 col-sm-6 col-md-4">
+          <div className="col-xxs-12 col-xs-6 col-sm-6 col-md-4">
             <PropertyCard toggleDetail={props.toggleDetail} id={5} />
           </div>
         </div>
